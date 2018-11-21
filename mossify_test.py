9b10f03bb5aad99796de62a3f1dcc30a73e55c33
Anton Bagliy <taccessviolation@gmail.com>
2018-11-21 22:54:47 +0000

    FEAT: quickly compare files by hash, use cmd arguments

--- mossify_test.py
+++ mossify_test.py
@@ -4,11 +4,27 @@
 import mosspy
 import os
 import time
+import argparse
+import hashlib
 
+def get_hash(fileName):
+    hash_md5 = hashlib.md5()
+    with open(fileName, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+   
 userid = 448215352
 
 m = mosspy.Moss(userid, "csharp")
 m.setDirectoryMode(1)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--hash", help="only quickly compare by hash", action = "store_true")
+parser.add_argument("--moss", help="use moss", action = "store_true")
+parser.add_argument("--all", help="use moss with all programs", action="store_true")
+parser.add_argument("--task", help="task to use")
+args = parser.parse_args()
 
 programs = {
     "Lexer":  ["Module1/Lexer.cs"],
@@ -25,7 +41,7 @@ programs = {
     "ASTParser": ["Module6/SimpleYacc.y",
         "Module6/SimpleLex.lex",
         "Module6/Main.cs",
-        "Module6/ProgramTree.cs"],
+        "Module6/ProgramTree.cs"],   
     "Visitors": ["Module7/SimpleLex.lex",
         "Module7/Main.cs",
         "Module7/ProgramTree.cs",
@@ -52,6 +68,31 @@ programs = {
 }
 
 
+programsToHash = {   
+    "Lexer":  ["Module1/Lexer.cs"],
+    "SimpleLexer": ["Module2/SimpleLangLexer/SimpleLexer.cs"],
+    "GeneratedLexer": ["Module3/SimpleLex.lex"],
+    "DescentParser": ["Module4/SimpleLangParser/SimpleLangParser.cs"],
+    "GeneratedParser": ["Module5/SimpleLex.lex",
+        "Module5/SimpleYacc.y"],
+    "ASTParser": ["Module6/SimpleYacc.y",
+        "Module6/ProgramTree.cs"],  
+    "Visitors": ["Module7/ProgramTree.cs",  
+        #"Module7/Visitors/ChangeVarIdVisitor.cs",
+        "Module7/Visitors/CommonlyUsedVarVisitor.cs",
+        "Module7/Visitors/CountCyclesOpVisitor.cs",
+        "Module7/Visitors/ExprComplexityVisitor.cs",
+        "Module7/Visitors/MaxIfCycleNestVisitor.cs",
+        "Module7/Visitors/MaxNestCyclesVisitor.cs",
+        "Module7/Visitors/PrettyPrintVisitor.cs"],   
+    "CodeGenerator": ["Module8/SimpleYacc.y",
+        "Module8/ProgramTree.cs",
+        "Module8/Visitors/PrettyPrintVisitor.cs",
+        "Module8/Visitors/GenCodeVisitors/GenCodeCreator.cs",
+        "Module8/Visitors/GenCodeVisitors/GenCodeVisitor.cs"]
+}
+
+  
 paths = [
         "Module1/Lexer.cs",
         "Module2/SimpleLangLexer/SimpleLexer.cs",
@@ -138,6 +179,41 @@ def checkProgram(programName):
     # Download whole report locally including code diff links
     mosspy.download_report(url, reportPath+programName+"/report"+timestamp+"/", connections=8)
     
+def checkProgramByHash(programName):
+    targetFilePaths = programsToHash[programName]
+    submissionPaths = os.listdir(baseDirPath)
+
+    files = []
+    baseFiles = {}
+    
+    for targetFilePath in targetFilePaths:
+        newBaseFile = baseProjectPath + targetFilePath
+        hash = get_hash(newBaseFile)
+        baseFiles[targetFilePath] = hash
+    
+    for submissionPath in submissionPaths:
+        for targetFilePath in targetFilePaths:
+            newFile = baseDirPath + submissionPath + "/" + targetFilePath
+            hash = get_hash(newFile)
+            isBase = True
+            if not baseFiles[targetFilePath] == hash:
+                isBase = False
+            if not isBase:
+                for f in files:
+                    if f[1] == hash:
+                        print 'copied: ', f[0], '|', newFile;
+                files.append((newFile, hash))
+    
 if __name__ == "__main__":
-    checkProgram("CodeGenerator")
-    #checkAllPrograms()     
+    if (args.hash):
+        if (args.all):
+            for program in programs:
+                checkProgramByHash(program)
+        else:
+            checkProgramByHash(args.task)
+    if (args.moss):
+        if (args.all):
+            checkAllPrograms()
+        else:
+            checkProgram(args.task)
+   
